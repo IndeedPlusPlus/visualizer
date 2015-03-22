a7d365582029873fc6db8d2201b54267a05981c2
Indeed <indeed@indeedblog.net>
2015-03-22 16:30:04 +0800

    create project from seed.

---
+++ master/js/app.init.js
@@ -0,0 +1,66 @@
+/*!
+ * 
+ */
+
+if (typeof $ === 'undefined') {
+    throw new Error('This application\'s JavaScript requires jQuery');
+}
+
+// APP START
+// ----------------------------------- 
+
+var App = angular.module('angle', [
+    'ngRoute',
+    'ngAnimate',
+    'ngStorage',
+    'ngCookies',
+    'pascalprecht.translate',
+    'ui.bootstrap',
+    'ui.router',
+    'oc.lazyLoad',
+    'cfp.loadingBar',
+    'ngSanitize',
+    'ngResource',
+    'ui.utils'
+]);
+
+App.run(["$rootScope", "$state", "$stateParams", '$window', '$templateCache', function ($rootScope, $state, $stateParams, $window, $templateCache) {
+    // Set reference to access them from any scope
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+    $rootScope.$storage = $window.localStorage;
+
+    // Uncomment this to disable template cache
+    /*$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+     if (typeof(toState) !== 'undefined'){
+     $templateCache.remove(toState.templateUrl);
+     }
+     });*/
+
+    // Scope Globals
+    // ----------------------------------- 
+    $rootScope.app = {
+        name: 'Visualizer',
+        description: 'General Purpose Data Visualizer',
+        year: ((new Date()).getFullYear()),
+        layout: {
+            isFixed: true,
+            isCollapsed: false,
+            isBoxed: false,
+            isRTL: false,
+            horizontal: false,
+            isFloat: false,
+            asideHover: false,
+            theme: null
+        },
+        useFullLayout: false,
+        hiddenFooter: false,
+        viewAnimation: 'ng-fadeInUp'
+    };
+    $rootScope.user = {
+        name: 'Indeed',
+        job: 'ng-developer',
+        picture: 'app/img/user/02.jpg'
+    };
+
+}]);
