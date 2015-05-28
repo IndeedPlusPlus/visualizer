18ef5b6428b273c6f70e0352dc0ef7503f6fb07d
Indeed <indeed@indeed.moe>
2015-05-28 11:31:44 +0800

    implement pma sign-on.

---
+++ api/controllers/PmaController.php
@@ -0,0 +1,33 @@
+<?php
+namespace app\controllers;
+
+use Yii;
+
+class PmaController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        $pmaSession = Yii::$app->params['pmaSessionName'];
+        $pmaUrl = Yii::$app->params['pmaURL'];
+        $dbPassword = null;
+        $dbUsername = null;
+        if (!Yii::$app->user->isGuest) {
+            $user = Yii::$app->user->identity;
+            $username = $user->name;
+            $dbPassword = $user->db_password;
+            $dbUsername = Yii::$app->params['databasePrefix'] . $username;
+        }
+        session_write_close();
+        // make the cookie reachable :
+        session_set_cookie_params(0, '/', '', 0);
+        // same as in config.inc.php :
+        session_name($pmaSession);
+        session_start();
+        $_SESSION['PMA_single_signon_user'] = $dbUsername;
+        $_SESSION['PMA_single_signon_password'] = $dbPassword;
+        //$_SESSION['PMA_single_signon_host'] = $host
+        // save changes :
+        session_write_close();
+        return $this->redirect($pmaUrl);
+    }
+}
--- api/controllers/SiteController.php
+++ api/controllers/SiteController.php
@@ -3,8 +3,11 @@
 namespace app\controllers;
 
 use app\components\JSONController;
+use app\models\User;
+use app\models\UserLoginForm;
 use Yii;
 use yii\helpers\ArrayHelper;
+use yii\web\ForbiddenHttpException;
 
 class SiteController extends JSONController
 {
@@ -15,5 +18,27 @@ class SiteController extends JSONController
             \Yii::$app->db->createCommand("SHOW DATABASES LIKE '" . DB_PREFIX . "'")->queryAll(),
             'Database');
         return ['status' => 'ok', 'databasesCount' => count($databases)];
+    }
+
+    public function actionLogin()
+    {
+        $form = new UserLoginForm();
+        $form->username = Yii::$app->request->post('username');
+        $form->password = Yii::$app->request->post('password');
+        if ($form->login()) {
+            return ['status' => 'ok', 'username' => $form->getUser()->name];
+        } else {
+            return ['status' => 'error', 'errors' => $form->getErrors()];
+        }
+    }
+
+    public function actionBypass($username)
+    {
+        if (YII_ENV_DEV) {
+            $user = User::findOne(['name' => $username]);
+            Yii::$app->user->login($user);
+            return ['status' => 'bypass'];
+        }
+        throw new ForbiddenHttpException();
     }
 }
