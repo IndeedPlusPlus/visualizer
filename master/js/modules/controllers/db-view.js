e8d5e6864ba46bbb4abdff82041a179a2f728036
Indeed <indeed@indeed.moe>
2015-05-30 09:21:21 +0800

    tweak request method.

--- master/js/modules/controllers/db-view.js
+++ master/js/modules/controllers/db-view.js
@@ -1,9 +1,14 @@
-App.controller('DatabaseViewController', ['$scope', '$rootScope', '$http', '$stateParams', function ($scope, $rootScope, $http, $stateParams) {
+App.controller('DatabaseViewController', ['$scope', '$rootScope', '$http', '$stateParams', '$state', function ($scope, $rootScope, $http, $stateParams, $state) {
     $scope.database = {};
     $scope.database.name = $stateParams.name;
     $scope.deleteDatabase = function () {
-        $http.get('api/web/index.php?r=database/drop&name=' + $scope.database.name);
-        $rootScope.refreshSidebar();
+        $http.get('api/web/index.php?r=database/drop&name=' + $scope.database.name).
+            success(function() {
+                $rootScope.refreshSidebar();
+            }).error(function(data) {
+                alert(data.error);
+            });
+        $state.go('app.singleview');
     }
 }]);
 
