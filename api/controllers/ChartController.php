2d6be3c8a7682ad5416c737340b967cf809f24ec
Indeed <indeed@indeed.moe>
2015-05-31 15:52:46 +0800

    implement api for chart display.

--- api/controllers/ChartController.php
+++ api/controllers/ChartController.php
@@ -3,20 +3,48 @@ namespace app\controllers;
 
 use app\components\JSONController;
 use app\models\Chart;
+use yii\filters\AccessControl;
 use yii\web\ForbiddenHttpException;
-use yii\web\HttpException;
 use yii\web\NotFoundHttpException;
 
 class ChartController extends JSONController
 {
-    public function actionIndex()
+    public function behaviors()
     {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'denyCallback' => function ($rule, $action) {
+                    throw new ForbiddenHttpException("not logged in.");
+                },
+                'rules' => [
+                    // allow authenticated users
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                    // everything else is denied by default
+                ],
+            ],
+        ];
+    }
 
+    public function actionIndex($database)
+    {
+        $user = \Yii::$app->user->identity;
+        $dbName = $user->mapDatabaseName($database);
+        return Chart::findAll(['db_name' => $dbName]);
     }
 
     public function actionCreate()
     {
 
+    }
+
+    public function actionView($chartId)
+    {
+        $chart = Chart::findOne($chartId);
+        return $chart;
     }
 
     public function actionData($chartId)
@@ -27,7 +55,7 @@ class ChartController extends JSONController
         if ($chart->owner->id != \Yii::$app->user->id)
             throw new ForbiddenHttpException();
         $user = \Yii::$app->user->identity;
-        $mysqli = new \mysqli('localhost' , $user->getDatabaseUser(), $user->db_password, $chart->db_name);
+        $mysqli = new \mysqli('localhost', $user->getDatabaseUser(), $user->db_password, $chart->db_name);
         return $mysqli->query($chart->query)->fetch_all(MYSQL_NUM);
     }
 }
