8aa5ff4b1449b4f8e9f12d4681b43748be09d7cd
Indeed <indeed@indeed.moe>
2016-03-16 16:39:49 +0800

    auto redirect to specified database.

--- api/controllers/PmaController.php
+++ api/controllers/PmaController.php
@@ -5,17 +5,21 @@ use Yii;
 
 class PmaController extends \yii\web\Controller
 {
-    public function actionIndex()
+    public function actionIndex($db = null)
     {
         $pmaSession = Yii::$app->params['pmaSessionName'];
         $pmaUrl = Yii::$app->params['pmaURL'];
         $dbPassword = null;
         $dbUsername = null;
+        $prefix = Yii::$app->params['databasePrefix'];
         if (!Yii::$app->user->isGuest) {
             $user = Yii::$app->user->identity;
             $username = $user->name;
+            $prefix = $prefix . $username . '_';
             $dbPassword = $user->db_password;
             $dbUsername = Yii::$app->params['databasePrefix'] . $username;
+        } else {
+             $db = null;
         }
         session_write_close();
         // make the cookie reachable :
@@ -28,6 +32,12 @@ class PmaController extends \yii\web\Controller
         $_SESSION['PMA_single_signon_host'] = Yii::$app->params['pmaHost'];
         // save changes :
         session_write_close();
+        if (strpos($pmaUrl, '?') === false) {
+            $pmaUrl = $pmaUrl . '?from=visualizer';
+        }
+        if ($db) {
+            $pmaUrl = $pmaUrl . '&' . http_build_query(['db' => $prefix . $db]);
+        }
         return $this->redirect($pmaUrl);
     }
 }
